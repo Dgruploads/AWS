308ec0c271e71f3a5a0a3375dbb244c25b5f9b0d
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2020-09-19 16:51:44 +0530

    Create copy.sh

---
+++ CodeDeploy/scripts/copy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+################################################################################################################
+# Please note this is an example copy path. You will need to change this according to the path on  your server.#
+################################################################################################################
+cp -R /opt/codedeploy-agent/deployment-root/c4b0429a-9560-4e1c-83b8-959d1951f89c/d-JWYH2BUK6/deployment-archive/target/sample-web-app.war /usr/local/tomcat/webapps/
