1e71aa317aa787feff19ae60fc1e2f980408cdfb
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2020-09-25 15:45:29 +0530

    Create create_s3.py

---
+++ Boto3/create_s3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import boto3
+
+#Using S3 service
+s3 = boto3.resource('s3')
+
+#Print the list of available buckets
+print ("Existing buckets :")
+for bucket in s3.buckets.all():
+	print (bucket.name)
+
+#Create a new bucket
+s3.create_bucket(Bucket='dgruploads-bucket')
+
+#Print the list of available bucket
+print ("Existing buckets :")
+for bucket in s3.buckets.all():
+	print (bucket.name)
+
+#Upload a new file
+data = open("index.html","rb")
+s3.Bucket("dgruploads-bucket").put_object(Key="index.html", Body=data)
